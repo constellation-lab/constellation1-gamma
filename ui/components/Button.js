86f14193fffa6ca10743c9f0968fa505799e5645
Emeka Onwuliri <conwuliri@gmail.com>
2023-05-15 10:25:40 +0100

    Create Button.js

---
+++ ui/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ children, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;

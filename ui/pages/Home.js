d113c8f26f9f71de8f8ee8c1d83d2a4785f8235a
Emeka Onwuliri <conwuliri@gmail.com>
2023-05-15 10:31:07 +0100

    Create Home.js

---
+++ ui/pages/Home.js
@@ -0,0 +1,47 @@
+
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import AnchorJS from "anchorjs";
+
+const Home = () => {
+  const [options, setOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  useEffect(() => {
+    // Get the list of options from the Cosm WASM contracts.
+    AnchorJS.query("option").then((result) => {
+      setOptions(result.data);
+    });
+  }, []);
+
+  const handleOptionSelect = (option) => {
+    // Set the selected option.
+    setSelectedOption(option);
+  };
+
+  const handleTrade = () => {
+    // Trade the selected option.
+    // ...
+  };
+
+  return (
+    <div>
+      <h1>OpynFinance</h1>
+      <ul>
+        {options.map((option) => (
+          <li key={option.id}>
+            <input
+              type="checkbox"
+              checked={option === selectedOption}
+              onChange={() => handleOptionSelect(option)}
+            />
+            {option.underlyingAsset} - {option.strikePrice}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleTrade}>Trade</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<Home />, document.getElementById("home"));
